5f55e0d4f1a97dd36a3c599e0106bd3f22d8036d
Giovanni Aceto <55949483+GioAceto@users.noreply.github.com>
2022-01-29 09:07:03 -0500

    add table output

--- client/src/components/art/artpiece/ArtPiece.js
+++ client/src/components/art/artpiece/ArtPiece.js
@@ -1,12 +1,47 @@
 import React from "react";
 
 const ArtPiece = ({ props }) => {
-  const { title, primaryImageSmall, department } = props;
+  const {
+    title,
+    primaryImageSmall,
+    department,
+    isHighlight,
+    culture,
+    period,
+    artistDisplayName,
+  } = props;
   console.log(props);
   return (
-    <div>
-      <h2>{title}</h2>
-    </div>
+    <table>
+      <caption>{title}</caption>
+      <tbody>
+        <tr>
+          <td>
+            <img src={primaryImageSmall} alt={title}></img>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>{artistDisplayName}</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>{department}</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>{culture}</p>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <p>{period}</p>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
